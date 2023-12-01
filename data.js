d457a6ec51b0bcc926ff25bab7b056fa3df872a5
EvaDiFer <evadiazfer16@gmail.com>
2023-12-01 17:44:25 +0100

    Primer commit

---
+++ data.js
@@ -0,0 +1,154 @@
+const PAINTING = [
+  {
+    id: 1,
+    name: 'Salvator Mundi',
+    artist: 'Leonardo da Vinci',
+    priceSold: 450,
+    yearSold: 2017,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701086393/PROYECTO-2-NEW/Leonardo_da_Vinci__Salvator_Mundi__c.1500__oil_on_walnut__45.4___65.6_cm_hcijqy.jpg',
+    style: 'Renaissance',
+  },
+  {
+    id: 2,
+    name: 'Interchange',
+    artist: 'Willem de Kooning',
+    priceSold: 300,
+    yearSold: 2015,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701429703/redimension/Photo_of_Interchanged_by_Willem_de_Kooning_x4rm9c_x1xjjo.jpg',
+    style: 'Abstract Expressionism',
+  },
+  {
+    id: 3,
+    name: "Les Femmes d'Alger",
+    artist: 'Pablo Picasso',
+    priceSold: 179,
+    yearSold: 2015,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701429701/redimension/Les_femmes_d_Alger__Picasso__version_O_xwluod_pgj7yj.jpg',
+    style: 'Cubism',
+  },
+  {
+    id: 4,
+    name: 'Nu Couché',
+    artist: 'Amedeo Modigliani',
+    priceSold: 170,
+    yearSold: 2015,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701429702/redimension/Modigliani-Nu-couche-PinacotecaAgnelli-dettaglio-1200x800_z6ok8u_meabtz.jpg',
+    style: 'Expressionism',
+  },
+  {
+    id: 5,
+    name: 'No. 5, 1948',
+    artist: 'Jackson Pollock',
+    priceSold: 140,
+    yearSold: '2006',
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701429700/redimension/293780_2x_fx0jox_jtyl4o.jpg',
+    style: 'Abstract Expressionism',
+  },
+  {
+    id: 6,
+    name: 'Women of Algiers (Version O)',
+    artist: 'Pablo Picasso',
+    priceSold: 160,
+    yearSold: 2015,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701429698/redimension/download_l6kkst_pfymok.jpg',
+    style: 'Cubism',
+  },
+  {
+    id: 7,
+    name: 'Nu Couché (sur le côté gauche)',
+    artist: 'Amedeo Modigliani',
+    priceSold: 157,
+    yearSold: 2018,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701087124/PROYECTO-2-NEW/58_dl4qls.jpg',
+    style: 'Expressionism',
+  },
+  {
+    id: 8,
+    name: 'Three Studies of Lucian Freud',
+    artist: 'Francis Bacon',
+    priceSold: 142,
+    yearSold: 2013,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701339921/PROYECTO-2-NEW/Three_Studies_of_Lucian_Freud_q2pwnc.jpg',
+    style: 'Surrealism',
+  },
+  {
+    id: 9,
+    name: 'Portrait of Adele Bloch-Bauer II',
+    artist: 'Gustav Klimt',
+    priceSold: 150,
+    yearSold: 2016,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701339961/PROYECTO-2-NEW/270px-Gustav_Klimt_047_ybajfx.jpg',
+    style: 'Art Nouveau',
+  },
+  {
+    id: 10,
+    name: 'Portrait of Adele Bloch-Bauer I',
+    artist: 'Gustav Klimt',
+    priceSold: 135,
+    yearSold: 2006,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701339977/PROYECTO-2-NEW/download_ot5fyd.jpg',
+    style: 'Art Nouveau',
+  },
+  {
+    id: 11,
+    name: 'Pendant portraits of Maerten Soolmans and Oopjen Coppit',
+    artist: 'Rembrandt',
+    priceSold: 160,
+    yearSold: 2015,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701429702/redimension/Pendant_portraits_of_Maerten_Soolmans_and_Oopjen_Coppit_pvxrvd_lvcjpx.jpg',
+    style: 'Baroque',
+  },
+  {
+    id: 12,
+    name: 'Portrait of Dr. Gachet',
+    artist: 'Vincent van Gogh',
+    priceSold: 82,
+    yearSold: 1990,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701340131/PROYECTO-2-NEW/download_aa43yi.jpg',
+    style: 'Post-Impressionism',
+  },
+  {
+    id: 13,
+    name: 'The Scream',
+    artist: 'Edvard Munch',
+    priceSold: 120,
+    yearSold: 2012,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701340159/PROYECTO-2-NEW/Edvard_Munch_-_The_Scream_-_Google_Art_Project_sbbldv.jpg',
+    style: 'Expressionism',
+  },
+  {
+    id: 14,
+    name: 'Portrait of a Halberdier',
+    artist: 'Pontormo',
+    priceSold: 35,
+    yearSold: 1989,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701340204/PROYECTO-2-NEW/Jacopo_Pontormo_-_Halberdier_-_WGA18101_voqrdk.jpg',
+    style: 'Mannerism',
+  },
+  {
+    id: 15,
+    name: 'Dora Maar au Chat',
+    artist: 'Pablo Picasso',
+    priceSold: 95,
+    yearSold: 2006,
+    image:
+      'https://res.cloudinary.com/djhjuxyes/image/upload/v1701340236/PROYECTO-2-NEW/Dora_Maar_Au_Chat_shr0ly.jpg',
+    style: 'Cubism',
+  },
+];
+
+export default PAINTING;
---
+++ main.js
@@ -0,0 +1,201 @@
+import './style.css';
+import PAINTING from './data';
+
+// Primer paso: Crear en el DOM cada producto que queremos mostrar en nuestro HTMl. En este caso son cuadros.
+
+const createPaintingElement = (artwork) => {
+  const paintingElement = document.createElement('div');
+  paintingElement.classList.add('painting');
+
+  // Crear y configurar la imagen
+  const imageContainer = document.createElement('div');
+  imageContainer.className = 'img-container';
+  const paintingImage = document.createElement('img');
+  paintingImage.src = artwork.image;
+  paintingImage.alt = artwork.name;
+  paintingImage.className = 'img-one';
+  imageContainer.appendChild(paintingImage);
+  paintingElement.appendChild(imageContainer);
+
+  // Crear y configurar los datos
+  const paintingName = document.createElement('h2');
+  paintingName.textContent = artwork.name;
+  paintingElement.appendChild(paintingName);
+
+  const paintingArtist = document.createElement('h3');
+  paintingArtist.textContent = artwork.artist;
+  paintingArtist.className = 'author';
+  paintingElement.appendChild(paintingArtist);
+
+  const priceSold = document.createElement('p');
+  priceSold.textContent = `$ ${artwork.priceSold}k`;
+  paintingElement.appendChild(priceSold);
+
+  const yearSold = document.createElement('p');
+  yearSold.textContent = `Year Sold: ${artwork.yearSold}`;
+  paintingElement.appendChild(yearSold);
+
+  return paintingElement;
+};
+
+// Como el resto de contenido es estático, he optado por crear templates y añadirlos al HTML
+
+const addHTML = (html) => {
+  document.body.innerHTML += html;
+};
+
+addHTML(`
+<header class="container">
+    <h1>ART GALLERY</h1>
+    <nav>
+      <ul class='nav-list'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#style'>ArtWorks</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+    </nav>
+    <button id="menu-toggle"><img src="https://res.cloudinary.com/djhjuxyes/image/upload/v1701366043/PROYECTO-2-NEW/painting_bzrwga.png" alt=""></button>
+
+  </header>
+`);
+
+addHTML(`
+<section>
+<p>From the Renaissance to the avant-garde, including pop art and modern art. These paintings set records with their prices in the art market.
+</p>
+
+<div id="filter-container">
+  <div id="style">
+    <select id="style-select">
+      <option value="All Styles">All Styles</option>
+      <option value="Cubism">Cubism</option>
+      <option value="Mannerism">Mannerism</option>
+      <option value="Expressionism">Expressionism</option>
+      <option value="Post-Impressionism">Post-Impressionism</option>
+      <option value="Baroque">Baroque</option>
+      <option value="Art Nouveau">Art Nouveau</option>
+      <option value="Surrealism">Surrealism</option>
+      <option value="Abstract Expressionism">Abstract Expressionism</option>
+      <option value="Renaissance">Renaissance</option>
+    </select>
+  </div>
+
+  <div id="filter-btns">
+    <input id="in-price" type="number" min="5" placeholder="Sale Price in M USD">
+    <button id="btn-search">Search</button>
+    <button id="btn-reset">Reset</button>
+  </div>
+</div>
+
+`);
+
+const divContainer = document.createElement('div');
+divContainer.id = 'artworks-container';
+
+PAINTING.forEach((artwork) => {
+  const artworkElement = createPaintingElement(artwork);
+  divContainer.appendChild(artworkElement);
+});
+
+document.body.appendChild(divContainer);
+
+// FUNCION PARA RENDERIZAR
+const renderArtwork = (artworks) => {
+  const divContainer = document.getElementById('artworks-container');
+  divContainer.innerHTML = '';
+  artworks.forEach((artwork) => {
+    const artworkElement = createPaintingElement(artwork);
+    divContainer.appendChild(artworkElement);
+  });
+};
+
+addHTML(`
+  <footer>
+    <div id="contact" class="footer-content">
+      <p>&copy; Art Gallery</p>
+      <p>Created by <a target="_blank" href="https://www.linkedin.com/in/eva-diaz-485880168/">Eva Díaz</a></p>
+      <p>Rock The Code🚀</p>
+
+    </div>
+  </footer>
+`);
+
+// FUNCION PARA EL PRECIO
+const filterByPrice = () => {
+  const maxPrice = Number(document.querySelector('#in-price').value);
+  const filteredArtworksByPrice = PAINTING.filter(
+    (artwork) => artwork.priceSold <= maxPrice
+  );
+
+  renderArtwork(filteredArtworksByPrice);
+};
+
+// ESCUCHADOR DE EVENTOS PARA EL FILTRO POR PRECIO
+
+const listenerPrice = document.querySelector('#btn-search');
+listenerPrice.addEventListener('click', filterByPrice);
+
+// FUNCION DE FILTRO POR ESTILO ARTÍSTICO
+const filterArtworksByStyle = (selectedStyle) => {
+  if (selectedStyle === 'All Styles') {
+    return PAINTING;
+  } else {
+    return PAINTING.filter((artwork) => artwork.style === selectedStyle);
+  }
+};
+// ESCUCHADOR DE EVENTOS PARA EL FILTRO POR ESTILO ARTISTICO
+const styleSelect = document.getElementById('style-select');
+styleSelect.addEventListener('change', () => {
+  const selectedStyle = styleSelect.value;
+  const filteredArtworks = filterArtworksByStyle(selectedStyle);
+  renderArtwork(filteredArtworks);
+});
+
+// FILTRO MULTIPLE(POR ESTILO ARTISTICO Y PRECIO)
+
+const FilterByStyleAndPrice = () => {
+  const maxPrice = Number(document.querySelector('#in-price').value);
+  const styleSelect = document.getElementById('style-select').value;
+
+  console.log('maxPrice:', maxPrice);
+  console.log('styleSelect:', styleSelect);
+
+  const filteredArtworks = PAINTING.filter(
+    (artwork) =>
+      artwork.priceSold <= maxPrice &&
+      (styleSelect === 'All Styles' || artwork.style === styleSelect)
+  );
+
+  renderArtwork(filteredArtworks);
+};
+
+const listenerStyleAndPrice = document.querySelector('#btn-search');
+listenerStyleAndPrice.addEventListener('click', FilterByStyleAndPrice);
+
+// RESET VALUES
+const priceInput = document.querySelector('#in-price');
+const resetButton = document.querySelector('#btn-reset');
+
+resetButton.addEventListener('click', () => {
+  renderArtwork(PAINTING);
+  priceInput.value = '';
+});
+
+const StyleOption = document.querySelector('#style-select');
+const resetInput = document.querySelector('#btn-reset');
+resetInput.addEventListener('click', () => {
+  StyleOption.value = 'All Styles';
+});
+
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.querySelector('.nav-list');
+
+menuToggle.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
+navList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    navList.classList.remove('active');
+  }
+});
